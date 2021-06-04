0a19920baa24358361a7e020c9e99587632fcefe
Martin Lexelius <martin.lexelius@creuna.se>
2021-06-04 06:03:19 +0200

    Move all js to js folder

---
+++ js/RandomGradient.js
@@ -0,0 +1,44 @@
+var RandomGradient = {
+
+  settings: {
+    htmlColorNames: [
+      'beige', 'blue', 'cyan', 'deepskyblue', 'gold', 'gray', 'green',
+      'hotpink', 'lavender', 'lime', 'magenta', 'red', 'orange', 
+      'pink', 'purple', 'silver', 'violet', 'yellow'
+    ],
+    angle: 135,
+    executeableWindow: '40rem'
+  },
+
+  init: function () {
+    s = this.settings;
+    this.paintGradient();
+  },
+
+  getRandomColor: function (clr) {
+    var x = Math.round( Math.random() * (s.htmlColorNames.length - 1) );
+    var clr = s.htmlColorNames[x];
+    return clr;
+  },
+
+  paintGradient: function () {
+    var color1 = this.getRandomColor();
+    var color2 = this.getRandomColor();
+
+    if (color1 == color2) {
+      color2 = this.getRandomColor();
+    }
+
+    var linearGradient = "linear-gradient(" + s.angle + "deg, " + color1 + ", " + color2 + ")";
+    document.body.style.backgroundImage = linearGradient;
+  }
+
+}
+
+if (window.innerWidth >= 640) {
+  RandomGradient.init();
+
+  setTimeout(function() {
+    RandomGradient.init();
+  }, 10000);
+}
